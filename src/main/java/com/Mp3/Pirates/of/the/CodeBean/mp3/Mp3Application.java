de25e9a0c0dfa8b96f40a5e3a7ac5624770c6b1d
ismael1987 <ismael1987@example.com>
2018-03-23 16:47:13 +0100

    mp3 home work

---
+++ src/main/java/com/Mp3/Pirates/of/the/CodeBean/mp3/Mp3Application.java
@@ -0,0 +1,12 @@
+package com.Mp3.Pirates.of.the.CodeBean.mp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mp3Application.class, args);
+	}
+}
